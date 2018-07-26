fcf79e27cb325cf911e2e25a6c38a2b6d4346d32
Radu Cristea <Radu.Cristea@endava.com>
2018-07-26 12:15:28 +0300

    Add a few basic tests

--- Connect4Tests/GameTests.cs
+++ Connect4Tests/GameTests.cs
@@ -7,13 +7,40 @@ namespace Tests
     public class GameTests
     {
         [Test]
-        public void Test1()
+        public void Play_WhenPlacingTheFirstDisk_ReturnsPlayerOneHasATurnMessage()
         {
             var game = new Game();
 
-            var message = game.play(0);
+            var message = game.play(0); // Player 1 has a turn
 
             Assert.AreEqual("Player 1 has a turn", message);
+        }
+
+        [Test]
+        public void Play_WhenPlacingTheSecondDisk_ReturnsPlayerTwoHasATurnMessage()
+        {
+            var game = new Game();
+
+            game.play(0);               // Player 1 has a turn
+            var message = game.play(0); // Player 2 has a turn
+
+            Assert.AreEqual("Player 2 has a turn", message);
+        }
+
+        [Test]
+        public void Play_WhenPlacingDiskToFullColumn_ReturnsColumnFullErrorMessage()
+        {
+            var game = new Game();
+
+            game.play(0);               // 1. Player 1 has a turn
+            game.play(0);               // 2. Player 2 has a turn
+            game.play(0);               // 3. Player 1 has a turn
+            game.play(0);               // 4. Player 2 has a turn
+            game.play(0);               // 5. Player 1 has a turn
+            game.play(0);               // 6. Player 2 has a turn
+            var message = game.play(0); // 7. Player 1 has a turn
+
+            Assert.AreEqual("Column full!", message);
         }
     }
 }
