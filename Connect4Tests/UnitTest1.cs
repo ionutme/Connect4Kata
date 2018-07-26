584ab52fde8c752de49665bcad37e9185b58a46b
Ionut Cristea <ionut.cristea@outlook.com>
2018-07-26 07:22:46 +0300

    Add first test

--- Connect4Tests/UnitTest1.cs
+++ Connect4Tests/UnitTest1.cs
@@ -1,18 +1,19 @@
 using NUnit.Framework;
+using Connect4Library;
 
 namespace Tests
 {
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            var game = new Game();
+
+            var message = game.play(0);
+
+            Assert.AreEqual("Player 1 has a turn", message);
         }
     }
 }
