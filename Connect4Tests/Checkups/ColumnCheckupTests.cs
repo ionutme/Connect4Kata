3d46455ed4be11aa54d5579adcc00b47378312aa
Radu Cristea <Radu.Cristea@endava.com>
2018-07-27 17:38:31 +0300

    Add basic tests for RowCheckup

--- Connect4Tests/Checkups/ColumnCheckupTests.cs
+++ Connect4Tests/Checkups/ColumnCheckupTests.cs
@@ -21,12 +21,11 @@ namespace Connect4Tests.Checkups
 
         [Test]
         [TestOf(typeof(ColumnCheckup))]
-        public void Has4Hits_WhenFistColumnHasFirstDiskDifferentFollowedBy4SameDisks_ReturnsTrue()
+        public void Has4Hits_WhenFistColumnHas1stDiskDifferentFollowedBy4SameDisks_ReturnsTrue()
         {
             var board = new Board();
             PlaceDisksOnColumn(1, 0, board, Game.Player.Two);
             PlaceDisksOnColumn(4, 0, board, Game.Player.One, 1);
-            PlaceDisksOnColumn(2, 1, board, Game.Player.Two);
 
             var columnCheckup = new ColumnCheckup(board);
 
@@ -35,13 +34,12 @@ namespace Connect4Tests.Checkups
 
         [Test]
         [TestOf(typeof(ColumnCheckup))]
-        public void Has4Hits_WhenFistColumnHasSecondDiskDifferentFromTheRest_ReturnsTrue()
+        public void Has4Hits_WhenFistColumnHas2ndDiskDifferentFromTheRest_ReturnsTrue()
         {
             var board = new Board();
             PlaceDisksOnColumn(1, 0, board, Game.Player.One);
             PlaceDisksOnColumn(1, 0, board, Game.Player.Two, 1);
             PlaceDisksOnColumn(4, 0, board, Game.Player.One, 2);
-            PlaceDisksOnColumn(3, 1, board, Game.Player.Two);
 
             var columnCheckup = new ColumnCheckup(board);
 
--- Connect4Tests/Checkups/RowCheckupTests.cs
+++ Connect4Tests/Checkups/RowCheckupTests.cs
@@ -21,7 +21,7 @@ namespace Connect4Tests.Checkups
 
         [Test]
         [TestOf(typeof(RowCheckup))]
-        public void Has4Hits_WhenFistRowHasFirstDiskDifferentFollowedBy4SameDisks_ReturnsTrue()
+        public void Has4Hits_WhenFistRowHas1stDiskDifferentFollowedBy4SameDisks_ReturnsTrue()
         {
             var board = new Board();
             PlaceDisksOnRow(1, Board.IndexMaxRows, board, Game.Player.Two);
@@ -30,6 +30,22 @@ namespace Connect4Tests.Checkups
             var rowCheckup = new RowCheckup(board);
 
             Assert.IsTrue(rowCheckup.Has4Hits((int)Game.Player.One));
+        }
+
+        [Test]
+        [TestOf(typeof(RowCheckup))]
+        public void Has4Hits_WhenFistRowHasThe2ndDiskDifferentFromTheRest_ReturnsTrue()
+        {
+            var board = new Board();
+            PlaceDisksOnRow(1, Board.IndexMaxRows, board, Game.Player.One);
+            PlaceDisksOnRow(1, Board.IndexMaxRows, board, Game.Player.Two, 1);
+            PlaceDisksOnRow(4, Board.IndexMaxRows, board, Game.Player.One, 2);
+
+            var rowCheckup = new RowCheckup(board);
+
+            Assert.IsTrue(rowCheckup.Has4Hits((int)Game.Player.One));
+
+            Console.WriteLine(GetBoardPrint(board));
         }
     }
 }
